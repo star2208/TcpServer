6ff54befe0b5e6f081c6d96987eaee39827cf38d
slfeng <slfeng@localhost.localdomain>
2016-07-19 14:50:42 +0800

    change file position of main.go fixed some typo in tcpserver.go

---
+++ src/tcpserverexe/main.go
@@ -0,0 +1,31 @@
+// TcpServer project main.go
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"tcpserver"
+)
+
+func wait_ctrl_c_and_stop(tcp_server *tcpserver.TcpServer) {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+
+	for sig := range c {
+		fmt.Printf("received ctrl+c (%v)\n", sig)
+
+		tcp_server.Stop()
+		fmt.Println("tcp server app stoped.")
+		os.Exit(0)
+	}
+}
+
+func main() {
+	fmt.Println("tcp server app started.")
+
+	tcp_server := tcpserver.NewTcpServer("virtual_tcp_server", 8888)
+	tcp_server.Start()
+
+	wait_ctrl_c_and_stop(tcp_server)
+}
